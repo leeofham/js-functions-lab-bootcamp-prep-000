59026bb9f95b52ae2e14ccc79f3a21ed66dd9de6
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-08 13:09:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays(){
+  return 'Happy holidays!';
+};
+
+function happyHolidaysTo(name){
+  var name = "";
+  return 'Happy holidays, ${name}!'
+}
+
+function happyHolidayTo(holiday, name){
+  var holiday = "",
+      name = ""
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function holidayCountdown(days, holiday){
+  var days = "",
+      name = ""
+  return 'It\'s ${days} days until ${holiday}!'
+}
